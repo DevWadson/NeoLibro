42a5a9d0bd3c53f8e60a246f1c61f5e21f08f1a7
DevWadson <marlonwadson.dev@gmail.com>
2026-07-03 16:19:08 -0300

    fix(gui/components): adiciona o arquivo __init__ ausente

---
+++ src/gui/components/__init__.py
@@ -0,0 +1,3 @@
+"""Componentes reutilizáveis da interface gráfica do NeoLibro."""
+from .cadastrar_menu_option_button import CadastrarMenuOptionButton
+from .procurar_menu_option_button import ProcurarMenuOptionButton
